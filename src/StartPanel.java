823a0adde484c635049613da8888a8f99b297f3c
MahraibFatima <mahraibfatima5@gmail.com>
2025-04-24 21:24:23 +0500

    exit button added

--- src/StartPanel.java
+++ src/StartPanel.java
@@ -5,6 +5,7 @@ import javax.swing.*;
 
 public class StartPanel extends JPanel implements ActionListener{
     PlayButton playButton;
+    PlayButton exitButton;
 
     StartPanel(int WIDTH, int HEIGHT,ImageIcon backgroundImg, JPanel container,CardLayout cardLayout){
 
@@ -15,15 +16,20 @@ public class StartPanel extends JPanel implements ActionListener{
         int buttonHeight = 50;
         int playButtonY = HEIGHT - (buttonHeight * 3);
 
+
         // Sets up the PLAY button
         String playButtonText = "PLAY";
         int playButtonX = (WIDTH/2) -(buttonWidth + 20);
         this.playButton = new PlayButton(playButtonText, playButtonX, playButtonY, buttonWidth, buttonHeight, container, cardLayout);
+        String playExitText = "Exit";
+        int exitButtonX = (WIDTH / 2) + 20;;
+        this.exitButton = new PlayButton(playExitText, exitButtonX, playButtonY, buttonWidth, buttonHeight, container, cardLayout);
 
         this.setLayout(new BorderLayout());
         this.add(startPanelBG);
         startPanelBG.setLayout(null);
         startPanelBG.add(this.playButton);
+        startPanelBG.add(this.exitButton);
 
     }
 
