d3bf98493ad638482530b3ec0e97f461c7a74ded
MahraibFatima <mahraibfatima5@gmail.com>
2025-05-07 00:23:47 +0500

    style for buttons and text field added

--- src/Styles.java
+++ src/Styles.java
@@ -1,8 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class TextFieldStyle {
-    public static JTextField getJTextField() {
+public class Styles {
+    public static JTextField getJTextFieldStyle() {
         JTextField field = new JTextField(5);
         field.setForeground(Color.BLACK);
         field.setOpaque(true);
@@ -10,4 +10,14 @@ public class TextFieldStyle {
         field.setBackground(new Color(255, 255, 255, 200));
         return field;
     }
+
+    public static void getStyleButton(JButton button) {
+        button.setFont(new Font("Arial", Font.BOLD, 16));
+        button.setPreferredSize(new Dimension(120, 40));
+        button.setBackground(new Color(70, 130, 180));
+        button.setForeground(Color.WHITE);
+        button.setFocusPainted(false);
+        button.setBorder(BorderFactory.createEmptyBorder(5, 15, 5, 15));
+    }
+
 }
